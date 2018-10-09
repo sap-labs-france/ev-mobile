0b2645fe493a2ad3f8292c4cf8608f2632d112ba
Liam FABIANO <liam.fabiano@sfr.fr>
2018-10-09 09:53:27 +0200

    Commit

--- src/components/Charger/index.js
+++ src/components/Charger/index.js
@@ -1,108 +1,31 @@
 import React, { Component } from "react";
-import { FlatList } from "react-native";
+import { FlatList, SectionList, Dimensions } from "react-native";
 import { Text, View, ListItem, Badge } from "native-base";
 
 import * as Animatable from "react-native-animatable";
 import { Grid, Col, Row } from "react-native-easy-grid";
 import styles from "./styles";
 
+const deviceWidth = Dimensions.get("window").width;
 class ChargerComponent extends Component {
 
-  nextChar = (c) => {
-    let i = (parseInt(c, 36) + 1 ) % 36;
-    return (!i * 10 + i).toString(36).toUpperCase();
-  }
-
-  _renderConnector = ({item}) => {
-    let alpha = "Z";
+  _renderConnector = ({item, index}) => {
+    let alpha = String.fromCharCode(65 + index);
     return (
-      <Row style={{backgroundColor: "##BDB76B", marginLeft: 15, marginRight: 15}}>
-        <Col style={{backgroundColor: "#FFA07A"}}>
-          {item.connectorId % 2 !== 0 ?
-            <Col>
-              <Row style={{marginTop: 10, alignSelf: "flex-start", backgroundColor: "#6495ED"}}>
-                {item.status === "Available" ?
-                  <Badge success>
-                    <Text>{this.nextChar(alpha)}</Text>
-                  </Badge>
-                : item.status === "Occupied" && item.currentConsumption === 0 ?
-                  <Badge danger>
-                    <Text>{this.nextChar(alpha)}</Text>
-                  </Badge>
-                : item.status === "Occupied" && item.currentConsumption !== 0 ?
-                  <Animatable.View animation="fadeIn" iterationCount={"infinite"} direction="alternate-reverse" duration={1000}>
-                    <Badge danger>
-                      <Text>{this.nextChar(alpha)}</Text>
-                    </Badge>
-                  </Animatable.View>
-                :
-                  <Badge danger>
-                    <Text>{this.nextChar(alpha)}</Text>
-                  </Badge>
-                }
-                <Row style={{backgroundColor: "#A52A2A"}}>
-                  <Col style={{backgroundColor: "#8A2BE2"}}>
-                    <Text style={{marginTop: 3, fontWeight: "bold"}}>{item.status !== "Available" && item.currentConsumption !== 0 ? parseFloat((item.currentConsumption / 1000).toFixed(1)) + " kW/h"  : item.status}</Text>
-                  </Col>
-                </Row>
-              </Row>
-              <Row style={{alignSelf: "flex-start", backgroundColor: "#008B8B"}}>
-                <Col>
-                  <Text style={{fontSize: 8.5, alignSelf: "flex-start"}}>Type ?</Text>
-                  <Text style={{fontSize: 8.5, alignSelf: "flex-start"}}>{Math.trunc(item.power / 1000)} kWMax</Text>
-                </Col>
-              </Row>
-            </Col>
-          :
-            <Col>
-              <Row style={{marginTop: 10, alignSelf: "flex-end", backgroundColor: "#98FB98"}}>
-                <Row style={{backgroundColor: "#BC8F8F"}}>
-                  <Col style={{backgroundColor: "	#EE82EE"}}>
-                    <Text style={{marginTop: 3, fontWeight: "bold"}}>{item.status !== "Available" && item.currentConsumption !== 0 ? parseFloat((item.currentConsumption / 1000).toFixed(1)) + " kW/h"  : item.status}</Text>
-                  </Col>
-                </Row>
-                {item.status === "Available" ?
-                  <Badge success>
-                    <Text>{this.nextChar(alpha)}</Text>
-                  </Badge>
-                : item.status === "Occupied" && item.currentConsumption === 0 ?
-                  <Badge danger>
-                    <Text>{this.nextChar(alpha)}</Text>
-                  </Badge>
-                : item.status === "Occupied" && item.currentConsumption !== 0 ?
-                  <Animatable.View animation="fadeIn" iterationCount={"infinite"} direction="alternate-reverse" duration={1000}>
-                    <Badge danger>
-                      <Text>{this.nextChar(alpha)}</Text>
-                    </Badge>
-                  </Animatable.View>
-                :
-                  <Badge danger>
-                    <Text>{this.nextChar(alpha)}</Text>
-                  </Badge>
-                }
-              </Row>
-              <Row style={{alignSelf: "flex-end", backgroundColor: "#008B8B"}}>
-                <Col>
-                  <Text style={{fontSize: 8.5, alignSelf: "flex-end"}}>Type ?</Text>
-                  <Text style={{fontSize: 8.5, alignSelf: "flex-end"}}>{Math.trunc(item.power / 1000)} kWMax</Text>
-                </Col>
-              </Row>
-            </Col>
-          }
-        </Col>
-      </Row>
+      undefined
     );
   }
 
-  _renderItem = ({item}) => {
+  _renderItem = ({item, index}) => {
     return (
       <ListItem>
-        <Grid style={{backgroundColor: "pink"}}>
-          <Row style={{backgroundColor: "#FF8C00"}}>
-            <Text>{item.id} - <Text style={{fontStyle: "italic"}}>{item.chargePointVendor}</Text></Text>
-          </Row>
-          <FlatList data={item.connectors} renderItem={this._renderConnector} keyExtractor={connector => connector.connectorId.toString()} />
-        </Grid>
+          <View style={{backgroundColor: "#87CEEB", flex: 1}}>
+            {/* Title */}
+            <View style={{backgroundColor: "#DDA0DD", paddingBottom: 5}}>
+              <Text>{item.id} - <Text style={{fontStyle: "italic"}}>{item.chargePointVendor}</Text></Text>
+            </View>
+          </View>
+            {/* <FlatList style={{flexDirection: "row"}} data={item.connectors} renderItem={this._renderConnector} keyExtractor={(connector, index) => connector.connectorId.toString()} /> */}
       </ListItem>
     );
   }
@@ -111,7 +34,7 @@ class ChargerComponent extends Component {
     let { items } = this.props;
     return (
       <View style={styles.container}>
-        <FlatList data={items} renderItem={this._renderItem} keyExtractor={item => item.id}/>
+        <FlatList data={items} renderItem={this._renderItem} keyExtractor={(item, index) => item.id}/>
       </View>
     );
   }
