3936698e7ac5595b95ceda961a710658c6d9ab49
Liam FABIANO <liam.fabiano@sfr.fr>
2018-10-29 11:48:37 +0100

    Graph: Can adapt Y axis values according to the current consumption

--- src/screens/Details/GraphDetails/index.js
+++ src/screens/Details/GraphDetails/index.js
@@ -9,14 +9,46 @@ const deviceHeight = Dimensions.get("window").height;
 
 class GraphDetails extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      charger: this.props.navigation.state.params.charger,
+      connector: this.props.navigation.state.params.connector,
+      alpha: this.props.navigation.state.params.alpha,
+      xCategory: [],
+      yCagegory: []
+    };
+  }
+
+  componentDidMount() {
+    this._fillYAxis();
+  }
+
+  _fillYAxis = () => {
+    const { connector } = this.state;
+    let yAxis = [];
+    let yMin = connector.currentConsumption - 10000 <= 0 ? 1000 : connector.currentConsumption - 5000;
+    let yMax = connector.currentConsumption - 10000 <= 0 ? 8000 : connector.currentConsumption + 2000;
+    console.log("yMin :", yMin);
+    console.log("yMax :", yMax);
+    for (var i = yMin; i <= yMax; i = i + 1 * 1000) {
+      yAxis.push((Math.round(i / 1000) + "000").toString());
+    }
+    this.setState({yCagegory: yAxis}, () => console.log(this.state.yCagegory));
+  }
+
   render() {
+    console.log(this.state.connector);
     return (
       <Container>
        <VictoryChart theme={VictoryTheme.material} width={deviceHeight} padding={{ top: 20, bottom: 80, left: 70, right: 20 }} >
           <VictoryArea
-            style={{ data: { fill: "#FFFFFF", fillOpacity: 0.7, stroke: "#c43a31", strokeWidth: 3 }}}
+            style={{ data: { fill: "#FFFFFF", fillOpacity: 0.6, stroke: "#c43a31", strokeWidth: 3 }}}
             domain={{ x: [0, 8], y: [0, 8] }}
-            categories={{ x: ["13h10", "13h20", "13h30", "13h40", "13h50", "14h00", "14h10", "14h20" ], y: ["1000", "2000", "3000", "4000", "5000", "6000", "7000", "8000"]}}
+            categories={{
+              x: ["13h10", "13h20", "13h30", "13h40", "13h50", "14h00", "14h10", "14h20"],
+              y: this.state.yCagegory
+            }}
             data={[
               { time: 0, charge: 0 },
               { time: 1, charge: 2 },
@@ -33,14 +65,16 @@ class GraphDetails extends Component {
           />
           <VictoryAxis
             label="Time"
-            style={{ axisLabel: { padding: 35, fill: "#FFFFFF" } }}
+            style={{
+              axisLabel: { padding: 35, fill: "#FFFFFF", fontWeight: "bold", fontSize: 15 }
+            }}
           />
           <VictoryAxis dependentAxis
             label="Charge in Watt(s)"
             style={{
-              axisLabel: { padding: 50, fill: "#FFFFFF" }
+              axisLabel: { padding: 55, fill: "#FFFFFF", fontWeight: "bold", fontSize: 15 }
             }}
-    /     >
+          />
        </VictoryChart>
       </Container>
     );
