611ae5b12d1c6bc78f66ead4222c771165729bed
Liam FABIANO <liam.fabiano@sfr.fr>
2018-12-11 16:07:08 +0100

    Rename to eMobility

--- ios/eMobilityTests/e_MobilityTests.m
+++ ios/eMobilityTests/e_MobilityTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface e_MobilityTests : XCTestCase
+@interface eMobilityTests : XCTestCase
 
 @end
 
-@implementation e_MobilityTests
+@implementation eMobilityTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
