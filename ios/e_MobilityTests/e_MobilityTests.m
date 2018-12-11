e41fc4e8c6798b04f43ccacc2df7a3b69bbc1bbf
FABIANO <serge.fabiano@sap.com>
2018-12-11 16:21:48 +0100

    Revert "Rename to eMobility"
    This reverts commit 611ae5b12d1c6bc78f66ead4222c771165729bed.

--- ios/e_MobilityTests/e_MobilityTests.m
+++ ios/e_MobilityTests/e_MobilityTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface eMobilityTests : XCTestCase
+@interface e_MobilityTests : XCTestCase
 
 @end
 
-@implementation eMobilityTests
+@implementation e_MobilityTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
