6c596c541b78d78d272b5ad375bf8d9289e55082
Jerome Benoit <jerome.benoit@sap.com>
2019-06-24 17:35:55 +0200

    Relink ReactNative libraries.
    And sync the version.
    
    Signed-off-by: Jerome Benoit <jerome.benoit@sap.com>

--- android/app/src/main/java/com/emobility/MainApplication.java
+++ android/app/src/main/java/com/emobility/MainApplication.java
@@ -3,6 +3,8 @@ package com.emobility;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import br.com.classapp.RNSensitiveInfo.RNSensitiveInfoPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNDeviceInfo(),
+            new RNI18nPackage(),
             new RNSensitiveInfoPackage()
       );
     }
