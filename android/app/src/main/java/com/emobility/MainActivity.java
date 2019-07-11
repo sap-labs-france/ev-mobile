09400373ca410135df3e4f6aab69593ab9ebf544
FABIANO <sergefabiano@free.fr>
2019-07-11 18:43:09 +0200

    Finished splash screen on Android

--- android/app/src/main/java/com/emobility/MainActivity.java
+++ android/app/src/main/java/com/emobility/MainActivity.java
@@ -10,7 +10,7 @@ import org.devio.rn.splashscreen.SplashScreen;
 public class MainActivity extends ReactActivity {
    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
+        SplashScreen.show(this, R.style.SplashScreenTheme);  // here
         super.onCreate(savedInstanceState);
     }
 
