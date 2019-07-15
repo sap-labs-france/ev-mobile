b3f9a43195825238c23f55efbfa7a6fb9999a5f6
FABIANO <sergefabiano@free.fr>
2019-07-15 10:50:54 +0200

    Adjust checkbox CSS for iOS

--- src/theme/variables/commonColor.js
+++ src/theme/variables/commonColor.js
@@ -31,10 +31,10 @@ export default {
   // CheckBox
   CheckboxRadius: platform === "ios" ? 13 : 0,
   CheckboxBorderWidth: platform === "ios" ? 1 : 2,
-  CheckboxPaddingLeft: platform === "ios" ? 4 : 2,
+  CheckboxPaddingLeft: platform === "ios" ? 5 : 2,
   CheckboxPaddingBottom: platform === "ios" ? 0 : 20,
   CheckboxIconSize: platform === "ios" ? 21 : 21,
-  CheckboxIconMarginTop: platform === "ios" ? undefined : 1,
+  CheckboxIconMarginTop: platform === "ios" ? 2 : 1,
   CheckboxFontSize: platform === "ios" ? 23 / 0.9 : 18,
   DefaultFontSize: 17,
   checkboxBgColor: "#fff",
