3ad6738e0375bbbca069262b898b02df7aa6dec1
FABIANO <serge.fabiano@sap.com>
2019-07-27 02:58:17 +0200

    Migrated to new Authorization framework

--- src/provider/SecurityProvider.js
+++ src/provider/SecurityProvider.js
@@ -1,86 +1,20 @@
 import Constants from "../utils/Constants";
-const Authorization = require("node-authorization").Authorization;
 
-const _debug = false;
 export default class SecurityProvider {
-  constructor(decodedToken) {
-    this.decodedToken = decodedToken;
+  constructor(loggedUser) {
+    this.loggedUser = loggedUser;
   }
 
   isAdmin() {
-    // Check
-    return this.decodedToken.role === Constants.ROLE_ADMIN;
+    return this.loggedUser.role === Constants.ROLE_ADMIN;
   }
 
   isBasic() {
-    // Check
-    return this.decodedToken.role === Constants.ROLE_BASIC;
+    return this.loggedUser.role === Constants.ROLE_BASIC;
   }
 
-  canStartTransaction(chargingStation) {
-    // Can perform stop?
-    if (
-      !this.canPerformActionOnChargingStation(
-        this.decodedToken,
-        chargingStation,
-        Constants.ACTION_START_TRANSACTION
-      )
-    ) {
-      // Ko
-      return false;
-    }
-    // Ok
-    return true;
-  }
-
-  canPerformActionOnChargingStation(chargingStation, action) {
-    // Check Charging Station
-    const result = this.canPerformAction(Constants.ENTITY_CHARGING_STATION, {
-      Action: action,
-      ChargingStationID: chargingStation.id
-    });
-
-    // Return
-    return (
-      result &&
-      this.canPerformActionOnSite(this.decodedToken, chargingStation, Constants.ACTION_READ)
-    );
-  }
-
-  canPerformActionOnSite(chargingStation, action) {
-    // Check Site?
-    if (chargingStation.siteArea) {
-      // Yes
-      return this.canPerformAction(Constants.ENTITY_SITE, {
-        Action: action,
-        SiteID: chargingStation.siteArea.siteID
-      });
-      // No: Must be an admin
-    } else if (this.isAdmin()) {
-      return true;
-      // No site management
-    } else {
-      // Ok
-      return true;
-    }
-  }
-
-  canPerformAction(entity, fieldNamesValues) {
-    // Set debug mode?
-    if (_debug) {
-      // Switch on traces
-      Authorization.switchTraceOn();
-    }
-    if (!this.authorizations) {
-      this.authorizations = new Authorization(this.decodedToken.role, this.decodedToken.auths);
-    }
-    // Check
-    if (this.authorizations.check(entity, fieldNamesValues)) {
-      // Authorized!
-      return true;
-    } else {
-      return false;
-    }
+  isDemo() {
+    return this.loggedUser.role === Constants.ROLE_DEMO;
   }
 
   isComponentOrganizationActive() {
@@ -88,11 +22,17 @@ export default class SecurityProvider {
   }
 
   isComponentActive(componentName) {
-    // Components provided
-    if (this.decodedToken.activeComponents) {
-      // Check components
-      return this.decodedToken.activeComponents.indexOf(componentName) !== -1;
+    if (this.loggedUser && this.loggedUser.activeComponents) {
+      return this.loggedUser.activeComponents.includes(componentName);
     }
     return false;
+  }
+
+  canUpdateChargingStation() {
+    return this._canAccess(Constants.ENTITY_CHARGING_STATION, Constants.ACTION_UPDATE);
+  }
+
+  _canAccess(resource: String, action: String) {
+    return this.loggedUser && this.loggedUser.scopes && this.loggedUser.scopes.includes(`${resource}:${action}`);
   }
 }
