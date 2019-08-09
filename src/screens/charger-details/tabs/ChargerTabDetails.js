5d480f01fbee8b81dd375b6f00242dbcdb08b7ec
FABIANO <sergefabiano@free.fr>
2019-08-09 18:03:49 +0200

    Hide tab bar if there is only one tab active

--- src/screens/charger-details/tabs/ChargerTabDetails.js
+++ src/screens/charger-details/tabs/ChargerTabDetails.js
@@ -151,40 +151,44 @@ export default class ChargerTabDetails extends BaseAutoRefreshScreen {
             rightAction={navigation.openDrawer}
             rightActionIcon={"menu"}
           />
-          <Tabs tabBarPosition="bottom" locked={true} initialPage={0}>
-            <Tab
-              heading={
-                <TabHeading style={style.tabHeader}>
-                  <Icon style={style.tabIcon} type="FontAwesome" name="bolt" />
-                </TabHeading>
-              }>
-              <ChargerConnectorDetails charger={charger} connector={connector} isAdmin={isAdmin} navigation={navigation} />
-            </Tab>
-            {connector.activeTransactionID && (isAuthorizedToStopTransaction || isAdmin) ? (
+          {!isAuthorizedToStopTransaction && !isAdmin ?
+            <ChargerConnectorDetails charger={charger} connector={connector} isAdmin={isAdmin} navigation={navigation} />
+          :
+            <Tabs tabBarPosition="bottom" locked={true} initialPage={0}>
               <Tab
                 heading={
                   <TabHeading style={style.tabHeader}>
-                    <Icon style={style.tabIcon} type="AntDesign" name="linechart" />
+                    <Icon style={style.tabIcon} type="FontAwesome" name="bolt" />
                   </TabHeading>
                 }>
-                <ChargerChartDetails transactionID={connector.activeTransactionID} isAdmin={isAdmin} navigation={navigation} />
+                <ChargerConnectorDetails charger={charger} connector={connector} isAdmin={isAdmin} navigation={navigation} />
               </Tab>
-            ) : (
-              undefined
-            )}
-            {isAdmin ? (
-              <Tab
-                heading={
-                  <TabHeading style={style.tabHeader}>
-                    <Icon style={style.tabIcon} type="MaterialIcons" name="info" />
-                  </TabHeading>
-                }>
-                <ChargerDetails charger={charger} connector={connector} isAdmin={isAdmin} navigation={navigation} />
-              </Tab>
-            ) : (
-              undefined
-            )}
-          </Tabs>
+              {connector.activeTransactionID && (isAuthorizedToStopTransaction || isAdmin) ? (
+                <Tab
+                  heading={
+                    <TabHeading style={style.tabHeader}>
+                      <Icon style={style.tabIcon} type="AntDesign" name="linechart" />
+                    </TabHeading>
+                  }>
+                  <ChargerChartDetails transactionID={connector.activeTransactionID} isAdmin={isAdmin} navigation={navigation} />
+                </Tab>
+              ) : (
+                undefined
+              )}
+              {isAdmin ? (
+                <Tab
+                  heading={
+                    <TabHeading style={style.tabHeader}>
+                      <Icon style={style.tabIcon} type="MaterialIcons" name="info" />
+                    </TabHeading>
+                  }>
+                  <ChargerDetails charger={charger} connector={connector} isAdmin={isAdmin} navigation={navigation} />
+                </Tab>
+              ) : (
+                undefined
+              )}
+            </Tabs>
+            }
         </BackgroundComponent>
       </ScrollView>
     );
