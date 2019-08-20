3207ec5db34bdd1c04c7ea904480a41307bb3cea
Jerome Benoit <jerome.benoit@sap.com>
2019-08-20 11:35:32 +0200

    Remove leftover firebase integration in iOS.
    Signed-off-by: Jerome Benoit <jerome.benoit@sap.com>

--- ios/eMobility/AppDelegate.m
+++ ios/eMobility/AppDelegate.m
@@ -18,7 +18,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [FIRApp configure];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"eMobility"
