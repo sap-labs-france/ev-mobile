9f02fa394716c3a4a7ec18ba35ccaabda7fc0adc
FABIANO <sergefabiano@free.fr>
2019-08-21 13:46:03 +0200

    Fixed iPad stylings

--- src/theme/components/Spinner.js
+++ src/theme/components/Spinner.js
@@ -3,7 +3,8 @@ import { scale } from "react-native-size-matters";
 
 export default () => {
   const spinnerTheme = {
-    height: scale(40)
+    width: scale(20),
+    height: scale(20)
   };
 
   return spinnerTheme;
