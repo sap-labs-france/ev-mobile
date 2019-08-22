35c2d11dfbfa9d56025ae13317e5499405cf0439
Jérôme Benoit <jerome.benoit@piment-noir.org>
2019-08-22 19:27:41 +0200

    Cleanup android version handling.
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- version-android.sh
+++ version-android.sh
@@ -4,12 +4,10 @@ PROJECT_DIR="android/app"
 GRADLE_FILE="build.gradle"
 GRADLE_RPATH="${PROJECT_DIR}/${GRADLE_FILE}"
 
-# PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
-
 PREVIOUS_BUILD_NUMBER=$(cat "${GRADLE_RPATH}" | grep versionCode | head -1 | awk -F' ' '{ print $2 }' | tr -d '[[:space:]]')
 BUILD_NUMBER=$(($PREVIOUS_BUILD_NUMBER + 1))
 
-# Update with new values
+# Update with new value
 sed -i\"\" "s/versionCode $PREVIOUS_BUILD_NUMBER/versionCode $BUILD_NUMBER/" ${GRADLE_RPATH}
 
 git add "${GRADLE_RPATH}"
