ba672f9219955ba84b5d9c6542791c7fd70f2c5e
Jérôme Benoit <jerome.benoit@piment-noir.org>
2019-10-14 21:16:37 +0200

    Use autolink for react-native-firebase.
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- android/app/src/main/java/com/emobility/MainApplication.java
+++ android/app/src/main/java/com/emobility/MainApplication.java
@@ -11,8 +11,8 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+// import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
+// import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 
 import java.util.List;
 
@@ -30,8 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
-      packages.add(new RNFirebaseMessagingPackage());
-      packages.add(new RNFirebaseNotificationsPackage());
+      // packages.add(new RNFirebaseMessagingPackage());
+      // packages.add(new RNFirebaseNotificationsPackage());
       return packages;
     }
 
