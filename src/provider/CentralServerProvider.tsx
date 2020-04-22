9293622ba55c6056056f689bb25e9d577bfdda64
FABIANO <sergefabiano@free.fr>
2020-04-22 11:16:05 +0200

    Switched to QA

--- src/provider/CentralServerProvider.tsx
+++ src/provider/CentralServerProvider.tsx
@@ -23,7 +23,7 @@ import SecurityProvider from './SecurityProvider';
 export default class CentralServerProvider {
   private debug: boolean = false;
   private captchaBaseUrl: string = 'https://evse.cfapps.eu10.hana.ondemand.com';
-  private centralRestServerServiceBaseURL: string = 'https://sap-ev-rest-server.cfapps.eu10.hana.ondemand.com';
+  private centralRestServerServiceBaseURL: string = 'https://sap-ev-rest-server-qa.cfapps.eu10.hana.ondemand.com';
   private centralRestServerServiceAuthURL: string = this.centralRestServerServiceBaseURL + '/client/auth';
   private centralRestServerServiceSecuredURL: string = this.centralRestServerServiceBaseURL + '/client/api';
   private captchaSiteKey: string = '6Lcmr6EUAAAAAIyn3LasUzk-0MpH2R1COXFYsxNw';
