547a7899ffb3150b74183eac16a1c9e6b0bfea56
FABIANO <sergefabiano@free.fr>
2020-06-01 00:40:05 +0200

    Enhanced consumption type

--- src/types/Consumption.tsx
+++ src/types/Consumption.tsx
@@ -2,8 +2,11 @@
 export default interface Consumption {
   date: Date;
   instantPower: number;
+  instantAmps: number;
   cumulatedConsumption: number;
+  cumulatedConsumptionAmps: number;
   stateOfCharge: number;
   cumulatedAmount: number;
   limitWatts: number;
+  limitAmps: number;
 }
