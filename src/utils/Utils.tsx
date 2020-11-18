47970e76546f9b681ac94f18f5d64c4a33e0b5cf
Jérôme Benoit <jerome.benoit@sap.com>
2020-11-18 21:45:45 +0100

    Final preparation for WhiteSource.
    + Spell fixes;
    + Typing.
    
    And I think that's enough for WhiteSource related work.
    
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- src/utils/Utils.tsx
+++ src/utils/Utils.tsx
@@ -7,9 +7,9 @@ import Address from 'types/Address';
 import { KeyValue } from 'types/Global';
 import validate from 'validate.js';
 
-import I18nManager from '../I18n/I18nManager';
-import ThemeManager from '../custom-theme/ThemeManager';
 import { buildCommonColor } from '../custom-theme/customCommonColor';
+import ThemeManager from '../custom-theme/ThemeManager';
+import I18nManager from '../I18n/I18nManager';
 import ChargingStation, { ChargePoint, ChargePointStatus, Connector, ConnectorType, CurrentType } from '../types/ChargingStation';
 import { RequestError } from '../types/RequestError';
 import { InactivityStatus } from '../types/Transaction';
@@ -122,7 +122,7 @@ export default class Utils {
     return values;
   }
 
-  public static isEmptyArray(array: any): boolean {
+  public static isEmptyArray(array: any[]): boolean {
     if (!array) {
       return true;
     }
