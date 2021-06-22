8d24c2cfe2db58075c877b81dfd36692f32aac3d
AlixH <alixhumbert@gmail.com>
2021-06-22 15:43:32 +0200

    formatting

--- src/screens/users/list/Users.tsx
+++ src/screens/users/list/Users.tsx
@@ -5,9 +5,7 @@ import React from 'react';
 import { View } from 'react-native';
 
 import HeaderComponent from '../../../components/header/HeaderComponent';
-import ItemsList, {
-  ItemSelectionMode,
-} from '../../../components/list/ItemsList';
+import ItemsList, { ItemSelectionMode } from '../../../components/list/ItemsList';
 import SimpleSearchComponent from '../../../components/search/simple/SimpleSearchComponent';
 import UserComponent from '../../../components/user/UserComponent';
 import I18nManager from '../../../I18n/I18nManager';
