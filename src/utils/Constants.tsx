513e18efa06e5235abf4ac5806e9d13896ba1e2f
rmangin-fr <remi.mangin@sap.com>
2021-09-28 15:50:31 +0200

    activate CZ

--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -3,8 +3,8 @@ import { PagingParams } from '../types/QueryParams';
 export default class Constants {
   public static readonly REST_RESPONSE_SUCCESS = 'Success';
 
-  public static readonly SUPPORTED_LOCALES = ['en_US', 'fr_FR', 'de_DE', 'es_ES', 'pt_PT', 'it_IT'];
-  public static readonly SUPPORTED_LANGUAGES = ['en', 'fr', 'de', 'es', 'pt', 'it'];
+  public static readonly SUPPORTED_LOCALES = ['en_US', 'fr_FR', 'de_DE', 'es_ES', 'pt_PT', 'it_IT', 'cz_CZ'];
+  public static readonly SUPPORTED_LANGUAGES = ['en', 'fr', 'de', 'es', 'pt', 'it', 'cz'];
   public static readonly DEFAULT_LOCALE = 'en_US';
   public static readonly DEFAULT_LANGUAGE = 'en';
 
