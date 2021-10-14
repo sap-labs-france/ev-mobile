24be9ee359eb893ed063b7e6f401fd5714529ca7
AlixH <alixhumbert@gmail.com>
2021-10-14 11:25:53 +0200

    Added ? operator to prevent crash

--- src/components/tag/TagComponent.tsx
+++ src/components/tag/TagComponent.tsx
@@ -60,7 +60,7 @@ export default class TagComponent extends React.Component<Props, State> {
           <Text numberOfLines={1} ellipsizeMode={'tail'} style={[style.text, style.tagDescription]}>
             {tag?.description}
           </Text>
-          {canReadUser && tag.user && (
+          {canReadUser && tag?.user && (
             <Text numberOfLines={1} ellipsizeMode={'tail'} style={[style.text, style.fullName]}>
               {userFullName}
             </Text>
