77f505ba3db618bfe540cf48f9c2bfe11f3691b0
AlixH <alixhumbert@gmail.com>
2021-10-21 15:07:04 +0200

    removed useless state var

--- src/screens/charging-stations/connector-details/ChargingStationConnectorDetails.tsx
+++ src/screens/charging-stations/connector-details/ChargingStationConnectorDetails.tsx
@@ -69,7 +69,6 @@ export interface State {
   showBillingErrorMessage?: boolean;
   transactionPending?: boolean;
   didPreparing?: boolean;
-  startTransactionDialogWasClosed?: boolean;
   showAdviceMessage?: boolean;
   transactionPendingTimesUp?: boolean;
   showChargingSettings?: boolean;
@@ -103,7 +102,7 @@ export default class ChargingStationConnectorDetails extends BaseAutoRefreshScre
       startTransactionNbTrial: 0,
       isPricingActive: false,
       refreshing: false,
-      showStartTransactionDialog: false,
+      showStartTransactionDialog: undefined,
       showStopTransactionDialog: false,
       selectedUser: null,
       selectedCar: null,
@@ -115,7 +114,6 @@ export default class ChargingStationConnectorDetails extends BaseAutoRefreshScre
       transactionPending: false,
       showAdviceMessage: false,
       didPreparing: false,
-      startTransactionDialogWasClosed: false,
       transactionPendingTimesUp: false,
       showChargingSettings: undefined
     };
@@ -358,8 +356,7 @@ export default class ChargingStationConnectorDetails extends BaseAutoRefreshScre
     if (
       startTransactionFromQRCode &&
       (connector?.status === ChargePointStatus.AVAILABLE || connector?.status === ChargePointStatus.PREPARING) &&
-      !buttonDisabled &&
-      !this.state.startTransactionDialogWasClosed
+      !buttonDisabled
     ) {
       showStartTransactionDialog = true;
     }
@@ -1219,14 +1216,14 @@ export default class ChargingStationConnectorDetails extends BaseAutoRefreshScre
       <DialogModal
         title={I18n.t('details.startTransaction')}
         withCloseButton={true}
-        close={() => this.setState({ showStartTransactionDialog: false, startTransactionDialogWasClosed: true })}
+        close={() => this.setState({ showStartTransactionDialog: false })}
         renderIcon={(style) => <Icon style={style} type={'MaterialIcons'} name={'play-circle-outline'} />}
         description={I18n.t('details.startTransactionMessage', { chargeBoxID: chargingStationID })}
         buttons={[
           {
             text: I18n.t('general.yes'),
             action: () =>
-              this.setState({ showStartTransactionDialog: false, startTransactionDialogWasClosed: true }, async () =>
+              this.setState({ showStartTransactionDialog: false }, async () =>
                 this.startTransaction()
               ),
             buttonTextStyle: modalCommonStyle.primaryButton,
@@ -1234,7 +1231,7 @@ export default class ChargingStationConnectorDetails extends BaseAutoRefreshScre
           },
           {
             text: I18n.t('general.no'),
-            action: () => this.setState({ showStartTransactionDialog: false, startTransactionDialogWasClosed: true }),
+            action: () => this.setState({ showStartTransactionDialog: false }),
             buttonTextStyle: modalCommonStyle.primaryButton,
             buttonStyle: modalCommonStyle.primaryButton
           }
