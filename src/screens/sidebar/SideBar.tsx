00f43d1919cc036d7f4449682b6931888bf079ce
AlixH <alixhumbert@gmail.com>
2022-08-22 18:32:42 +0200

    Improved sidebar first shot

--- src/screens/sidebar/SideBar.tsx
+++ src/screens/sidebar/SideBar.tsx
@@ -1,9 +1,19 @@
 import { StackActions } from '@react-navigation/native';
 import I18n from 'i18n-js';
 import moment from 'moment';
-import { Icon, IIconProps, Pressable } from 'native-base';
+import { Icon, IIconProps } from 'native-base';
 import React from 'react';
-import { Image, ImageStyle, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import {
+  Image,
+  ImageStyle,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  TouchableHighlight,
+  TouchableHighlightProps
+} from 'react-native';
 import { CheckVersionResponse } from 'react-native-check-version';
 import DeviceInfo from 'react-native-device-info';
 
@@ -22,6 +32,20 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import { scale } from 'react-native-size-matters';
 
 export interface Props extends BaseProps {}
+
+function SidebarEntry(props: TouchableHighlightProps): React.ReactElement {
+  const style = computeStyleSheet();
+  const commonColor = Utils.getCurrentCommonColor();
+  return (
+    <TouchableHighlight underlayColor={commonColor.listItemBackground} {...props}>
+      <View style={style.links}>{props.children}</View>
+    </TouchableHighlight>
+  );
+}
+function SidebarIcon(props: IIconProps): React.ReactElement {
+  const commonColor = Utils.getCurrentCommonColor();
+  return <Icon color={commonColor.textColor} size={scale(22)} {...props} />;
+}
 
 interface State {
   userToken?: UserToken;
@@ -43,8 +67,6 @@ export default class SideBar extends React.Component<Props, State> {
 
   public constructor(props: Props) {
     super(props);
-   // this.componentFocusUnsubscribe = this.props.navigation?.addListener('focus', () => this.componentDidFocus());
-   // this.componentBlurUnsubscribe = this.props.navigation?.addListener('blur', () => this.componentDidFocus());
     this.state = {
       userToken: null,
       tenantName: '',
@@ -115,122 +137,129 @@ export default class SideBar extends React.Component<Props, State> {
     this.props.navigation.navigate(container, { screen, params, key: Utils.randomNumber().toString() });
   };
 
-
   public render() {
     const style = computeStyleSheet();
     const commonColor = Utils.getCurrentCommonColor();
     const { userToken, tenantName, isComponentOrganizationActive, showAppUpdateDialog, appVersion, tenantLogo } = this.state;
     const user = { firstName: userToken?.firstName, name: userToken?.name, id: userToken?.id } as User;
-    const SidebarIcon = (props: IIconProps) =>  <Icon color={commonColor.textColor} size={scale(22)} {...props} />;
     // Get logo
     return (
-      <SafeAreaView style={style.sidebar}>
-        <View style={style.header}>
-          {tenantLogo && <Image source={{ uri: tenantLogo }} style={style.logo as ImageStyle} />}
-          <Text numberOfLines={1} style={style.tenantName}>
-            {tenantName}
-          </Text>
-          <Pressable
-            disabled={!appVersion?.needsUpdate}
-            onPress={() => this.setState({ showAppUpdateDialog: true })}
-            style={style.versionContainer}>
-            <Text style={style.versionText}>{`${I18n.t('general.version')} ${DeviceInfo.getVersion()}`}</Text>
-            {appVersion?.needsUpdate && (
-              <View style={style.newVersionContainer}>
-                <SidebarIcon style={style.newVersionIcon} as={MaterialIcons} name={'update'} />
-                <Text style={style.newVersionText}>{I18n.t('appUpdate.appUpdateDialogTitle')}</Text>
+      <View style={style.container}>
+        <SafeAreaView style={style.sidebar}>
+          <View style={style.header}>
+            <View style={style.tenantContainer}>
+              {tenantLogo && <Image source={{ uri: tenantLogo }} style={style.logo as ImageStyle} />}
+              <Text numberOfLines={3} style={style.tenantName}>
+                {tenantName}
+              </Text>
+            </View>
+            <View style={style.versionContainer}>
+              {!appVersion?.needsUpdate ? (
+                <Text style={style.versionText}>{`${I18n.t('general.version')} ${DeviceInfo.getVersion()}`}</Text>
+              ) : (
+                <TouchableOpacity style={style.newVersionContainer}>
+                  <SidebarIcon style={style.newVersionIcon} as={MaterialIcons} name={'update'} />
+                  <Text style={style.newVersionText}>{I18n.t('appUpdate.appUpdateDialogTitle')}</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+            {showAppUpdateDialog && <AppUpdateDialog appVersion={appVersion} close={() => this.setState({ showAppUpdateDialog: false })} />}
+          </View>
+          <View style={{width: '100%'}}>
+            <ScrollView style={style.drawerContent}>
+              <View style={style.linkContainer}>
+                <View style={style.sidebarSection}>
+                  <SidebarEntry onPress={() => this.props.navigation.navigate('QRCodeScanner')}>
+                    <SidebarIcon as={MaterialIcons} name="qr-code-scanner" />
+                    <Text style={style.linkText}>{I18n.t('sidebar.qrCodeScanner')}</Text>
+                  </SidebarEntry>
+                </View>
+                <View style={style.sidebarSection}>
+                  {isComponentOrganizationActive && (
+                    <SidebarEntry onPress={() => this.navigateTo('SitesNavigator', 'Sites')}>
+                      <SidebarIcon as={MaterialIcons} name="store-mall-directory" />
+                      <Text style={style.linkText}>{I18n.t('sidebar.sites')}</Text>
+                    </SidebarEntry>
+                  )}
+                  <SidebarEntry onPress={() => this.navigateTo('ChargingStationsNavigator', 'ChargingStations')}>
+                    <SidebarIcon as={MaterialIcons} name="ev-station" />
+                    <Text style={style.linkText}>{I18n.t('sidebar.chargers')}</Text>
+                  </SidebarEntry>
+                </View>
+                <View style={style.sidebarSection}>
+                  <SidebarEntry
+                    onPress={() => this.navigateTo('TransactionHistoryNavigator', 'TransactionsHistory')}>
+                    <SidebarIcon as={MaterialCommunityIcons} name="history" />
+                    <Text style={style.linkText}>{I18n.t('sidebar.transactionsHistory')}</Text>
+                  </SidebarEntry>
+                  <SidebarEntry
+                    onPress={() => this.navigateTo('TransactionInProgressNavigator', 'TransactionsInProgress')}>
+                    <SidebarIcon as={MaterialIcons} name="play-arrow" />
+                    <Text style={style.linkText}>{I18n.t('sidebar.transactionsInProgress')}</Text>
+                  </SidebarEntry>
+                  <SidebarEntry onPress={() => this.navigateTo('StatisticsNavigator', 'Statistics')}>
+                    <SidebarIcon as={MaterialIcons} name="assessment" />
+                    <Text style={style.linkText}>{I18n.t('sidebar.statistics')}</Text>
+                  </SidebarEntry>
+                </View>
+                <View style={style.sidebarSection}>
+                  {this.securityProvider?.canListUsers() && (
+                    <SidebarEntry onPress={() => this.navigateTo('UsersNavigator', 'Users')}>
+                      <SidebarIcon as={MaterialIcons} name="people" />
+                      <Text style={style.linkText}>{I18n.t('sidebar.users')}</Text>
+                    </SidebarEntry>
+                  )}
+                  {this.securityProvider?.canListTags() && (
+                    <SidebarEntry onPress={() => this.navigateTo('TagsNavigator', 'Tags')}>
+                      <SidebarIcon as={MaterialCommunityIcons} name="credit-card" />
+                      <Text style={style.linkText}>{I18n.t('sidebar.badges')}</Text>
+                    </SidebarEntry>
+                  )}
+                  {this.securityProvider?.canListCars() && this.securityProvider?.isComponentCarActive() && (
+                    <SidebarEntry onPress={() => this.navigateTo('CarsNavigator', 'Cars')}>
+                      <SidebarIcon as={MaterialIcons} name="directions-car" />
+                      <Text style={style.linkText}>{I18n.t('sidebar.cars')}</Text>
+                    </SidebarEntry>
+                  )}
+                </View>
+                <View style={style.sidebarSection}>
+                  {this.securityProvider?.canListPaymentMethods() && this.securityProvider?.isComponentBillingActive() && (
+                    <SidebarEntry
+                      onPress={() => this.navigateTo('PaymentMethodsNavigator', 'PaymentMethods')}>
+                      <SidebarIcon as={MaterialIcons} name="payments" />
+                      <Text style={style.linkText}>{I18n.t('sidebar.paymentMethods')}</Text>
+                    </SidebarEntry>
+                  )}
+                  {this.securityProvider?.canListInvoices() && this.securityProvider?.isComponentBillingActive() && (
+                    <SidebarEntry onPress={() => this.navigateTo('InvoicesNavigator', 'Invoices')}>
+                      <SidebarIcon as={MaterialIcons} name="receipt" />
+                      <Text style={style.linkText}>{I18n.t('sidebar.invoices')}</Text>
+                    </SidebarEntry>
+                  )}
+                </View>
+                <SidebarEntry
+                  onPress={() => this.navigateTo('ReportErrorNavigator', 'ReportError')}>
+                  <SidebarIcon color={commonColor.dangerLight} as={MaterialIcons} name="error-outline" />
+                  <Text style={[style.linkText, { color: commonColor.dangerLight }]}>{I18n.t('sidebar.reportError')}</Text>
+                </SidebarEntry>
               </View>
-            )}
-          </Pressable>
-          {showAppUpdateDialog && <AppUpdateDialog appVersion={appVersion} close={() => this.setState({ showAppUpdateDialog: false })} />}
-        </View>
-        <View style={style.border} />
-        <ScrollView style={style.drawerContent}>
-          <View style={style.linkContainer}>
-            <TouchableOpacity style={style.links} onPress={() => this.props.navigation.navigate('QRCodeScanner')}>
-              <SidebarIcon as={MaterialIcons} name="qr-code-scanner" />
-              <Text style={style.linkText}>{I18n.t('sidebar.qrCodeScanner')}</Text>
-            </TouchableOpacity>
-            {isComponentOrganizationActive && (
-              <TouchableOpacity style={style.links} onPress={() => this.navigateTo('SitesNavigator', 'Sites')}>
-                <SidebarIcon as={MaterialIcons} name="store-mall-directory" />
-                <Text style={style.linkText}>{I18n.t('sidebar.sites')}</Text>
-              </TouchableOpacity>
-            )}
-            <TouchableOpacity style={[style.links]} onPress={() => this.navigateTo('ChargingStationsNavigator', 'ChargingStations')}>
-              <SidebarIcon as={MaterialIcons} name="ev-station" />
-              <Text style={style.linkText}>{I18n.t('sidebar.chargers')}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={style.links}
-              onPress={() => this.navigateTo('TransactionHistoryNavigator', 'TransactionsHistory')}>
-              <SidebarIcon as={MaterialCommunityIcons} name="history" />
-              <Text style={style.linkText}>{I18n.t('sidebar.transactionsHistory')}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={style.links}
-              onPress={() => this.navigateTo('TransactionInProgressNavigator', 'TransactionsInProgress')}>
-              <SidebarIcon as={MaterialIcons} name="play-arrow" />
-              <Text style={style.linkText}>{I18n.t('sidebar.transactionsInProgress')}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={style.links} onPress={() => this.navigateTo('StatisticsNavigator', 'Statistics')}>
-              <SidebarIcon as={MaterialIcons} name="assessment" />
-              <Text style={style.linkText}>{I18n.t('sidebar.statistics')}</Text>
-            </TouchableOpacity>
-            {this.securityProvider?.canListUsers() && (
-              <TouchableOpacity style={style.links} onPress={() => this.navigateTo('UsersNavigator', 'Users')}>
-                <SidebarIcon as={MaterialIcons} name="people" />
-                <Text style={style.linkText}>{I18n.t('sidebar.users')}</Text>
-              </TouchableOpacity>
-            )}
-            {this.securityProvider?.canListTags() && (
-              <TouchableOpacity style={style.links} onPress={() => this.navigateTo('TagsNavigator', 'Tags')}>
-                <SidebarIcon as={MaterialCommunityIcons} name="credit-card" />
-                <Text style={style.linkText}>{I18n.t('sidebar.badges')}</Text>
-              </TouchableOpacity>
-            )}
-            {this.securityProvider?.canListCars() && this.securityProvider?.isComponentCarActive() && (
-              <TouchableOpacity style={style.links} onPress={() => this.navigateTo('CarsNavigator', 'Cars')}>
-                <SidebarIcon as={MaterialIcons} name="directions-car" />
-                <Text style={style.linkText}>{I18n.t('sidebar.cars')}</Text>
-              </TouchableOpacity>
-            )}
-            {this.securityProvider?.canListPaymentMethods() && this.securityProvider?.isComponentBillingActive() && (
-              <TouchableOpacity
-                style={style.links}
-                onPress={() => this.navigateTo('PaymentMethodsNavigator', 'PaymentMethods')}>
-                <SidebarIcon as={MaterialIcons} name="payments" />
-                <Text style={style.linkText}>{I18n.t('sidebar.paymentMethods')}</Text>
-              </TouchableOpacity>
-            )}
-            {this.securityProvider?.canListInvoices() && this.securityProvider?.isComponentBillingActive() && (
-              <TouchableOpacity style={style.links} onPress={() => this.navigateTo('InvoicesNavigator', 'Invoices')}>
-                <SidebarIcon as={MaterialIcons} name="receipt" />
-                <Text style={style.linkText}>{I18n.t('sidebar.invoices')}</Text>
-              </TouchableOpacity>
-            )}
-            <TouchableOpacity
-              style={style.links}
-              onPress={() => this.navigateTo('ReportErrorNavigator', 'ReportError')}>
-              <SidebarIcon color={commonColor.dangerLight} as={MaterialIcons} name="error-outline" />
-              <Text style={[style.linkText, { color: commonColor.dangerLight }]}>{I18n.t('sidebar.reportError')}</Text>
-            </TouchableOpacity>
+            </ScrollView>
           </View>
-        </ScrollView>
-        <View style={style.bottomContainer}>
-          <View style={style.avatarContainer}>
-            <UserAvatar user={user} navigation={this.props.navigation} />
+          <View style={style.bottomContainer}>
+            <View style={style.avatarContainer}>
+              <UserAvatar user={user} navigation={this.props.navigation} />
+            </View>
+            <View style={style.rightContainer}>
+              <Text style={style.userName}>
+                {Utils.buildUserName(user)}
+              </Text>
+              <TouchableOpacity style={style.logoutContainer} onPress={async () => this.logoff()}>
+                <Text style={style.logoutText}>{I18n.t('authentication.logOut')}</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <View style={style.rightContainer}>
-            <Text style={style.userName}>
-              {Utils.buildUserName(user)}
-            </Text>
-            <TouchableOpacity style={style.logoutContainer} onPress={async () => this.logoff()}>
-              <Text style={style.logoutText}>{I18n.t('authentication.logOut')}</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </View>
     );
   }
 }
