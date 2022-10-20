39bcc4abb4d6b6f28b98d783a51d97f2c72142bf
AlixH <alixhumbert@gmail.com>
2022-10-20 16:35:35 +0200

    Use new Recaptcha key

--- src/config/Configuration.tsx
+++ src/config/Configuration.tsx
@@ -6,7 +6,7 @@ export default class Configuration {
   public static readonly AWS_REST_ENDPOINT_PROD = `${Configuration.SERVER_URL_PREFIX}e-mobility-group.com`;
   public static readonly AWS_REST_ENDPOINT_QA = 'https://qa-e-mobility-group.com';
 
-  public static readonly CAPTCHA_SITE_KEY = '6Lcmr6EUAAAAAIyn3LasUzk-0MpH2R1COXFYsxNw';
+  public static readonly CAPTCHA_SITE_KEY = '6LdkE1ohAAAAANYfRirQEJHc2H5AjtQNOdsUYv2B';
   public static readonly DEFAULT_ENDPOINT_CLOUD_ID = 'aws';
   public static readonly ENDPOINT_CLOUDS: EndpointCloud[] = [
     { id: Configuration.DEFAULT_ENDPOINT_CLOUD_ID, name: 'Amazon Web Service', endpoint: Configuration.AWS_REST_ENDPOINT_PROD }
