c29a2b43722654a6449a3c435cd747d9a9b5a485
AlixH <alixhumbert@gmail.com>
2022-10-26 15:36:28 +0200

    Upgraded Android conf for Firebase v6

--- android/app/src/main/java/com/emobility/MainApplication.java
+++ android/app/src/main/java/com/emobility/MainApplication.java
@@ -10,8 +10,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
-import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -29,8 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
-      packages.add(new RNFirebaseMessagingPackage());
-      packages.add(new RNFirebaseNotificationsPackage());
       return packages;
     }
 
