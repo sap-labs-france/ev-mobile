f874257d262505415138ba9cbf51a75a442c4039
AlixH <alixhumbert@gmail.com>
2022-11-17 17:53:17 +0100

    Config for handling notification in background/quit states

--- ios/eMobility/AppDelegate.m
+++ ios/eMobility/AppDelegate.m
@@ -67,7 +67,8 @@ static void InitializeFlipper(UIApplication *application) {
  // }];
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"eMobility" initialProperties:nil];
+  NSDictionary *appProperties = [RNFBMessagingModule addCustomPropsToUserProps:nil withLaunchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"eMobility" initialProperties:appProperties];
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
