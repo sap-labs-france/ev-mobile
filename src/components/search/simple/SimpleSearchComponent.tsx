84de5684bf766ad896f3febe565c29fc19874082
AlixH <alixhumbert@gmail.com>
2022-12-20 18:24:30 +0100

    Allow searchbar to be populated from outside

--- src/components/search/simple/SimpleSearchComponent.tsx
+++ src/components/search/simple/SimpleSearchComponent.tsx
@@ -12,10 +12,13 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 
 export interface Props extends BaseProps {
   onChange: (search: string) => void;
-  containerStyle?: {}
+  containerStyle?: {};
+  searchText?: string;
 }
 
-interface State {}
+interface State {
+  searchText: string;
+}
 
 export default class SimpleSearchComponent extends React.Component<Props, State> {
   public state: State;
@@ -24,7 +27,9 @@ export default class SimpleSearchComponent extends React.Component<Props, State>
 
   public constructor(props: Props) {
     super(props);
-    this.state = {};
+    this.state = {
+      searchText: this.props.searchText
+    };
   }
 
   public setState = (
@@ -37,12 +42,18 @@ export default class SimpleSearchComponent extends React.Component<Props, State>
   public searchHasChanged(searchText: string) {
     const { onChange } = this.props;
     // Call the function
-    onChange(searchText);
+    this.setState({searchText}, () => onChange(searchText));
   }
 
   public clearSearch() {
     this.textInput.clear();
     this.searchHasChanged('');
+  }
+
+  public componentDidUpdate(prevProps: Readonly<Props>, prevState: Readonly<State>, snapshot?: any) {
+    if (this.props.searchText !== this.state.searchText) {
+      this.setState({searchText: this.props.searchText});
+    }
   }
 
   public render() {
@@ -59,6 +70,7 @@ export default class SimpleSearchComponent extends React.Component<Props, State>
           selectionColor={commonColor.textColor}
           style={style.inputField}
           autoCorrect={false}
+          value={this.props.searchText}
           placeholder={I18n.t('general.search')}
           placeholderTextColor={commonColor.placeholderTextColor}
           onChangeText={(searchText) => this.searchHasChanged(searchText)}
